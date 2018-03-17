6385e496598614a09428cd60855c2e31c2aa32dc
AGhiuta <alex.ghiuta@gmail.com>
2018-03-17 21:17:58 +0200

    grab snapshots from camera

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -5,7 +5,7 @@ from geometry_msgs.msg import PoseStamped, Pose
 from styx_msgs.msg import TrafficLightArray, TrafficLight
 from styx_msgs.msg import Lane
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 from light_classification.tl_classifier import TLClassifier
 import tf
 import cv2
@@ -71,6 +71,14 @@ class TLDetector(object):
         self.has_image = True
         self.camera_image = msg
         light_wp, state = self.process_traffic_lights()
+
+        try:
+            cv2_img = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+        else:
+            if msg.header.seq % 10 == 0:
+                cv2.imwrite('/home/user2/Documents/snaps/{}.jpeg'.format(msg.header.seq / 10), cv2_img)
 
         '''
         Publish upcoming red lights at camera frequency.
